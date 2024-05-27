33c7207acc0160628345dd9be0270226400accd8
EdgrantHS <edgrant.h@gmail.com>
2024-05-27 13:41:32 +0700

    interupt

--- FinproSSF.ino
+++ FinproSSF.ino
@@ -1,8 +1,23 @@
 extern "C"{
   void start();
+  void check_or();
+}
+
+void setupButton() {
+  // Set interrupt on falling edge
+  EICRA |= (1 << ISC01);
+  EICRA &= ~(1 << ISC00);
+  // Enable interrupt
+  EIMSK |= (1 << INT0);
 }
 
 int main(){
   init();
+  setupButton();
   start();
+}
+
+// ISR untuk button
+ISR(INT0_vect) {
+  check_or(); // Toggle LED PB1
 }
