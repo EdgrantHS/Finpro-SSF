78b2af1a180d72ec3d9f167c5d416bebfe461a47
EdgrantHS <edgrant.h@gmail.com>
2024-05-27 13:43:19 +0700

    delete beberapa file

--- FinproSSF.ino
+++ FinproSSF.ino
@@ -1,14 +1,24 @@
 extern "C"{
   void start();
   void check_or();
+  void check_and();
 }
 
 void setupButton() {
-  // Set interrupt on falling edge
-  EICRA |= (1 << ISC01);
+  // setup int 0, check_or button
+  EICRA |= (1 << ISC01); // Set interrupt on falling edge
   EICRA &= ~(1 << ISC00);
   // Enable interrupt
   EIMSK |= (1 << INT0);
+
+  // Enable global interrupt
+  sei();
+
+  // setup int 0, check_or button
+  EICRA |= (1 << ISC11); // Set interrupt on falling edge
+  EICRA &= ~(1 << ISC10);
+  // Enable interrupt
+  EIMSK |= (1 << INT1);
 }
 
 int main(){
@@ -17,7 +27,12 @@ int main(){
   start();
 }
 
-// ISR untuk button
+// ISR untuk button or
 ISR(INT0_vect) {
-  check_or(); // Toggle LED PB1
+  check_or(); 
+}
+
+// ISR untuk button and
+ISR(INT1_vect) {
+  check_and();
 }
